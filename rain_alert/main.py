8330c7d76e1e02ed61e247d9aea18d707227b766
Chris Parsonson <cjparsonson@googlemail.com>
2022-11-15 19:43:12 +0000

    Updates

--- rain_alert/main.py
+++ rain_alert/main.py
@@ -1,10 +1,8 @@
 import requests
-<<<<<<< HEAD
 import os
 from dotenv import load_dotenv
 from twilio.rest import Client
 load_dotenv()
-
 account_sid = os.getenv("TWILIOSID2")
 auth_token = os.getenv("TWILIOKEY2")
 
