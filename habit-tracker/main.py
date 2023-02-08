d497f4fd5ea45f1b913a5a2f4ddc2983e6c9d63b
Chris Parsonson <cjparsonson@googlemail.com>
2023-02-08 21:57:54 +0000

    Finish exercises

--- habit-tracker/main.py
+++ habit-tracker/main.py
@@ -1,13 +1,15 @@
 import requests
 import os
+from datetime import datetime, timedelta
 from dotenv import load_dotenv
 load_dotenv()
 # Global Variables
 USERNAME = os.getenv("PIXELA_USERNAME")
 TOKEN = os.getenv("PIXELA_TOKEN")
+GRAPH_ID = "graph1"
 PIXELA_ENDPOINT = "https://pixe.la/v1/users"
 GRAPH_ENDPOINT = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs"
-PIXEL_POST_ENDPOINT = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs/graph1"
+PIXEL_POST_ENDPOINT = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs/{GRAPH_ID}"
 
 # Pixela signup - comment out after first run
 user_params = {
@@ -19,26 +21,52 @@ user_params = {
 # response = requests.post(url=PIXELA_ENDPOINT, json=user_params)
 # response.raise_for_status()
 graph_config = {
-    "id": "graph1",
+    "id": GRAPH_ID,
     "name": "Programming Graph",
     "unit": "Hour",
     "type": "float",
     "color": "momiji"
     }
 
-pixel_post_params = {
-    "date": "20221120",
-    "quantity": "1"
-}
-
 headers = {
     "X-USER-TOKEN": os.getenv("PIXELA_TOKEN")
 }
+
+today = datetime.now()
+yesterday = datetime.now() - timedelta(1)
+print(today.strftime("%Y%m%d"))
+print(yesterday.strftime("%Y%m%d"))
+
+pixel_post_params = {
+    "date": yesterday.strftime("%Y%m%d"),
+    "quantity": "1"
+}
+
+
 # response = requests.post(url=GRAPH_ENDPOINT, json=graph_config, headers=headers)
 # print(response.text)
+#
+# response = requests.post(url=PIXEL_POST_ENDPOINT, json=pixel_post_params, headers=headers)
+# if "retry" in response.text:
+#     response = requests.post(url=PIXEL_POST_ENDPOINT, json=pixel_post_params, headers=headers)
 
-response = requests.post(url=PIXEL_POST_ENDPOINT, json=pixel_post_params, headers=headers)
+# Update Yesterday
+# Get yesterday, format and create params
+yesterday_formatted = yesterday.strftime("%Y%m%d")
+pixel_update_params = {
+    "quantity": "2"
+}
+
+# Build new endpoint
+UPDATE_ENDPOINT = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs/{GRAPH_ID}/{yesterday_formatted}"
+
+# Make PUT request
+response = requests.put(url=UPDATE_ENDPOINT, json=pixel_update_params, headers=headers)
 print(response.text)
+if "retry" in response.text:
+    response = requests.put(url=UPDATE_ENDPOINT, json=pixel_update_params, headers=headers)
+    print(response.text)
+
 
 
 
